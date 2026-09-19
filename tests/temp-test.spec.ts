6c590a2beb4eeead2c59b266dae080348390f8b2
wishinfinite <selvag004@gmail.com>
2026-09-19 15:18:08 +0530

    trigger fresh run

--- tests/temp-test.spec.ts
+++ tests/temp-test.spec.ts
@@ -4,6 +4,7 @@ test("Temp Test",async ({page,loginPage,commonPage,gotoUrl})=>{
 
     await page.waitForURL(process.env.HOME_PAGE_URL!)
     console.log(await page.title())
+    console.log("Successfully run")
 
 
 })
